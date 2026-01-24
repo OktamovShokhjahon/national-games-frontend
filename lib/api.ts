c54b8a8128071c31058925915302b09aafa071b6
oktamovshokhjahon <oktamovshokhjahon@gmail.com>
2026-01-24 23:03:31 +0500

    api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://national-games-backend.onrender.com';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://national-games-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
